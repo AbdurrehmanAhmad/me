9ab1361e47afc10f59efe70771afdff23e66d770
AbdurrehmanAhmad <151956537+AbdurrehmanAhmad@users.noreply.github.com>
2023-11-24 10:14:31 -0800

    me

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("running");
+document.querySelector(".ham").addEventListener("click",()=>{
+    document.querySelector(".sidebar").classList.toggle('sidebarGO')
+})
+document.querySelector("click")
